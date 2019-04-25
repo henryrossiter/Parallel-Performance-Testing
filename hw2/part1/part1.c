9b0c78228d3b13e023c8e73d938cb0bc0f1dbada
Henry Rossiter <henry.rossiter@utexas.edu>
2019-04-25 13:41:54 -0500

    mpi part 2 lets go

--- hw2/part1/part1.c
+++ hw2/part1/part1.c
@@ -28,23 +28,55 @@ void initialize(float *arr, uint64_t n){
         }
 }
 
-void smooth(float *inArr, float *outArr, uint64_t n, float a, float b, float c){
-
-        for (uint64_t i = 1; i < n-1; i++)
+void smooth(float *inArr, float *outArr, float *colLeft, float *colRight, float *rowAbove, float *rowBelow, uint64_t n, float a, float b, float c){
+        for (uint64_t i = 0; i < n; i++) // y coordinate
         {
-                for (uint64_t j = 1; j < n-1; j++)
+                for (uint64_t j = 0; j < n; j++) //x coordinate
                 {
-                        outArr[i*n+j] = a * (inArr[(i - 1)*n+j - 1] + inArr[(i - 1)*n+j + 1] + inArr[(i + 1)*n+j - 1] + inArr[(i + 1)*n+j + 1]) + b * (inArr[(i)*n+j - 1] + inArr[(i)*n+j + 1] + inArr[(i - 1)*n+j] + inArr[(i + 1)*n+j]) + c * (inArr[(i)*n+j]);
+                        //top border conditions
+                        outArr[i*n+j] = c * inArr[(i)*n+j];
+                        if (i == 0) {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (rowAbove[j] + inArr[(i + 1)*n+j]) );
+                        } else if (i == n - 1) {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (inArr[(i - 1)*n+j] + rowBelow[j]) );
+                        } else {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (inArr[(i - 1)*n+j] + inArr[(i + 1)*n+j]) );
+                        }
+
+                        // left and right border conditions
+                        if (j == 0) {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (colLeft[i] + inArr[(i)*n+j + 1] );
+                        } else if (j == n - 1) {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (inArr[(i)*n+j - 1] + colRight[i] );
+                        } else {
+                          outArr[i*n+j] = outArr[i*n+j] + ( b * (inArr[(i)*n+j - 1] + inArr[(i)*n+j + 1] );
+                        }
                 }
         }
 }
 
-void count(float *arr, uint64_t n, float threshold, uint64_t *cnt){
+void count(int *my_coord, int gridSize, float *arr, uint64_t n, float threshold, uint64_t *cnt){
         uint64_t localcnt = *cnt;
-        
-        for (uint64_t i = 1; i < n-1; i++)
+
+        uint64_t i = 0;
+        uint64_t j = 0;
+        uint64_t iend = n;
+        uint64_t jend = n;
+
+        //dont count the edges of the edge processes
+        if (my_coord[0] == 0) {
+          i = 1
+        }if (my_coord[0] == gridSize - 1) {
+          iend = n - 1
+        }if (my_coord[1] == 0) {
+          j = 1
+        }if (my_coord[1] == gridSize - 1) {
+          jend = n - 1
+        }
+
+        for (i = 0; i < iend; i++)
         {
-                for (uint64_t j = 1; j < n-1; j++)
+                for (j = 0; j < jend; j++)
                 {
                         if (arr[i*n+j] < threshold)
                         {
@@ -55,86 +87,182 @@ void count(float *arr, uint64_t n, float threshold, uint64_t *cnt){
         *cnt = localcnt;
 
 }
+
 int main( int argc, char *argv[] )
 {
-        const float a = 0.05;
-        const float b = 0.1;
-        const float c = 0.4;
-        const float t = 0.1;
-        const uint64_t n = 67;// 98306;
-        float *x; //pointer
-        float *y; //pointer
-        uint64_t memPerProc = n*n*sizeof(float);
-        
+  const float a = 0.05;
+  const float b = 0.1;
+  const float c = 0.4;
+  const float t = 0.1;
+  const uint64_t n = 67;
+  const uint64_t elementsPerProc = n*n;
+  const uint64_t memPerProc = elementsPerProc*sizeof(float);
+  int i;
+
+
+  float *x; //pointer
+  float *y; //pointer
+
 	double start;
-        double times[6];
-	
-        int myid, numprocs;
-    
+  double times[6];
+
+  int myid, numprocs;
+
 	MPI_Status status;
 	MPI_Init(&argc,&argv);
-    	MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
-    	MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-	int elementsPerProc = n*n/numprocs;
-	int colsPerProc = n/numprocs;
+  MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
 	printf("Hello world from %d of %d processors\n",myid, numprocs);
 
 
+  //mpi cartesian grid
+  //create a grid of processes
+  int ndim = 2;
+
+  int dimensions[2];
+  dimensions[0] = gridSize;
+  dimensions[1] = gridSize;
+
+  MPI_Comm comm2d;
+
+  int periodic[2];
+  periodic[0] = 0;
+  periodic[1] = 0;
+
+  int rank_2d; //literal int
+  int my_coord[2]; //pointer
+
+  MPI_Cart_create(MPI_COMM_WORLD, ndim, dimensions, periodic, 1, &comm2d);
+  MPI_Cart_coords(comm2d, myid, ndim, my_coord);
+  MPI_Cart_rank(comm2d, my_coord, &rank_2d);
+  printf("I am %d: (%d,%d); originally %d\n", rank_2d, my_coord[0], my_coord[1], myid );
+
 	start = gtod_timer(); //start clock
 
-        x = (float*)malloc(memNeeded/numprocs);
+  x = (float*)malloc(memPerProc);
 
-        times[0] = gtod_timer();
+  times[0] = gtod_timer();
 
-        y = (float*)malloc(memNeeded);
+  y = (float*)malloc(memPerProc);
 
-        times[1] = gtod_timer();
+  times[1] = gtod_timer();
+  //start of initialization kernel a
 
 	if (myid == 0) {
-		//for each slave, make a random array and send it to the proc
-		printf("elements per proc: %d",elementsPerProc);
-		
+		//for each process, make a random array and send it
+
 		for (int islave = 1; islave < numprocs; islave++) {
 			initialize(x, elementsPerProc);
 			MPI_Send(x, elementsPerProc, MPI_INTEGER, islave, MTAG1, MPI_COMM_WORLD);
 		}
-		
+
+    //initialize "my own" array
+    initialize(x, elementsPerProc);
+
 	} else {
 		MPI_Recv(x, elementsPerProc, MPI_INTEGER, 0, MTAG1, MPI_COMM_WORLD, &status);
 	}
 
 	printf(" Thread %d 's array starts with %d",myid, x[0]);
 
+  //end of initialization kernel a
+  times[2] = gtod_timer();
 
-        times[2] = gtod_timer();
+  int gridSize = sqrt(numprocs)
 
-	if ( myid != 0 ){
-		smooth(x, y, n, a, b, c);
-	}
+  float colLeft[gridSize];
+  float colRight[gridSize];
+  float rowAbove[gridSize];
+  float rowBelow[gridSize];
 
-        times[3] = gtod_timer();
+  int ind;
+
+  //store current processes left column in an array
+  ind = 0;
+  float myColLeft[gridSize];
+  for (i = 0; i< elementsPerProc; i++) {
+    if (i % n == 0) {
+      myLeftColumn[ind] = x[i];
+      ind = ind + 1
+    }
+  }
+
+  //store current processes Right column in an array
+  ind = 0;
+  float myColRight[gridSize];
+  for (i = 0; i < elementsPerProc; i++) {
+    if (i % n == n - 1) {
+      myRightColumn[ind] = x[i];
+      ind = ind + 1
+    }
+  }
+
+  float myRowTop[gridSize];
+  for (i=0; i < n; i++){
+    myTopRow[i] = x[i]
+  }
+
+  float myRowBottom[gridSize];
+  for (i=0; i < n; i++){
+    myBottomRow[i] = x[elementsPerProc - n + i]
+  }
+
+  MPI_Cart_shift(comm2d,0,+1,&rank_2d,&rank_right);
+  MPI_Cart_shift(comm2d,0,-1,&rank_2d,&rank_left);
+  MPI_Cart_shift(comm2d,1,+1,&rank_2d,&rank_up);
+  MPI_Cart_shift(comm2d,1,-1,&rank_2d,&rank_down);
+
+  int irequest = 0;
+  MPI_Request *requests = malloc(8*sizeof(MPI_Request));
+
+  //send to rank_left
+  if (my_coord[0] > 0) {
+    MPI_Isend( myColLeft,1,MPI_FLOAT,rank_left,  0,comm, requests+irequest++);
+    MPI_Irecv( colLeft, 1,MPI_FLOAT, rank_left,  0,comm, requests+irequest++);
+  }
+
+  //send to rank_right
+  if (my_coord[0] < gridSize - 1) {
+    MPI_Isend( myColRight ,1,MPI_FLOAT,rank_right, 0,comm, requests+irequest++);
+    MPI_Irecv( colRight, 1,MPI_FLOAT, rank_right, 0,comm, requests+irequest++);
+  }
+
+  //send to rank_up
+  if (my_coord[1] > 0) {
+    MPI_Isend( myRowTop,1,MPI_FLOAT,rank_up, 0,comm, requests+irequest++);
+    MPI_Irecv( rowAbove, 1,MPI_FLOAT, rank_up, 0,comm, requests+irequest++);
+  }
+
+  //send to rank_down
+  if (my_coord[1] < gridSize - 1) {
+    MPI_Isend( myRowBottom,1,MPI_FLOAT,rank_down, 0,comm, requests+irequest++);
+    MPI_Irecv( rowBelow, 1,MPI_FLOAT, rank_down, 0,comm, requests+irequest++);
+  }
+
+	smooth(x, y, colLeft, colRight, colUp, colDown, n, a, b, c);
+
+  times[3] = gtod_timer();
+
 	uint64_t countx = 0;
-        uint64_t county = 0;
+  uint64_t county = 0;
 	uint64_t countxtot = 0;
 	uint64_t countytot = 0;
-	
-	if (myid != 0){
-		count(x, elementsPerProc, t, &countx);
-		MPI_Send(&countx, 1, MPI_INTEGER, 0, MTAG2, MPI_COMM_WORLD);	
-	} else {
-		//receive all counts
-		//summ all counts
-		MPI_Reduce(&countx,&countxtot,1, MPI_INTEGER, MPI_SUM, MTAG2, MPI_COMM_WORLD);
-	}
 
-        times[4] = gtod_timer();
 
-        count(y, n, t, &county);
+	count(my_coord, gridSize, y, n, t, &county);
 
-        times[5] = gtod_timer();
-	
-	if (myid == 0) {	
+  MPI_Reduce(&countx,&countxtot,1, MPI_INTEGER, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  times[4] = gtod_timer();
+
+  count(my_coord, gridSize, y, n, t, &county);
+
+  MPI_Reduce(&county,&countytot,1, MPI_INTEGER, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  times[5] = gtod_timer();
+
+	if (myid == 0) {
 		printf("Summary\n");
         	printf("-----------------------------------------------\n");
         	printf("Number of threads used in parallel region:   %u\n", numprocs);
@@ -150,17 +278,16 @@ int main( int argc, char *argv[] )
         	printf("Number   of elements below threshold (y)     %u\n", county);
         	printf("Fraction of elements below threshold (y)     %1.8f\n",(float)county/(n-2)/(n-2));
 
-		printf ("Process         time(s)    resolution: 1.0e-04\n");
-        	printf ("-----------------------\n");
-        	printf ("Allocate-x:     %4.3f\n", times[0]-start);
-        	printf ("Allocate-y:     %4.3f\n", times[1]-times[0]);
-        	printf ("Initialize-x:   %4.3f\n", times[2]-times[1]);
-        	printf ("Smooth:         %4.3f\n", times[3]-times[2]);
-        	printf ("Count-x:        %4.3f\n", times[4]-times[3]);
-        	printf ("Count-y:        %4.3f\n", times[5]-times[4]);
-	}	
+	printf ("Process         time(s)    resolution: 1.0e-04\n");
+      	printf ("-----------------------\n");
+      	printf ("Allocate-x:     %4.3f\n", times[0]-start);
+      	printf ("Allocate-y:     %4.3f\n", times[1]-times[0]);
+      	printf ("Initialize-x:   %4.3f\n", times[2]-times[1]);
+      	printf ("Smooth:         %4.3f\n", times[3]-times[2]);
+      	printf ("Count-x:        %4.3f\n", times[4]-times[3]);
+      	printf ("Count-y:        %4.3f\n", times[5]-times[4]);
+	}
 
 	MPI_Finalize();
         return 0;
 }
-
